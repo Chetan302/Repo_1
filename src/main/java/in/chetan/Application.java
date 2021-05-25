fc5fa91e460566c52a44077a672bba047aad7ccd
chetan302 <chaudharichetan943@gmail.com>
2021-05-25 10:43:08 +0530

    Sting variable added

--- src/main/java/in/chetan/Application.java
+++ src/main/java/in/chetan/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+   private String status= "";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
