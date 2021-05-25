60d3a6ac358356d11cc91847e33d651d8e603ccb
chetan302 <chaudharichetan943@gmail.com>
2021-05-25 10:38:18 +0530

    Rest URL modified

--- src/main/java/in/chetan/Application.java
+++ src/main/java/in/chetan/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    
+    private static final String REST_URL = "www.lobOne.com/login";
     
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
