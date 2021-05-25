b03d1169d2cab4f8e61603adfb6691bc36f870f4
chetan302 <chaudharichetan943@gmail.com>
2021-05-25 10:50:18 +0530

    nothing to say

--- src/main/java/in/chetan/Application.java
+++ src/main/java/in/chetan/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	private static final String REST_URL = "www.lobOne.com/login";
+	private int a = 10;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		
